1a089ef64cdd5f5960af61799c8ef56f35a7cf7f
Cristian Pérez Orozco <cristian.h.perez.o@gmail.com>
2021-06-29 17:34:24 +0100

    restore

--- src/config/axios.js
+++ src/config/axios.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const clienteAxios = axios.create({
-    baseURL: 'https://product-crud-project.herokuapp.com/api'
+    baseURL: 'http://localhost:4000/'
 });
 
 export default clienteAxios;
