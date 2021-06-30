38119e96770c2eead65db2e0c13cde43e50e9140
Cristian Pérez Orozco <cristian.h.perez.o@gmail.com>
2021-06-30 19:21:44 +0100

    prueba1

--- src/config/axios.js
+++ src/config/axios.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const clienteAxios = axios.create({
-    baseURL: 'http://localhost:4000/'
+    baseURL: 'https://prueba-backend-json.herokuapp.com/'
 });
 
 export default clienteAxios;
